85d8d85d8e789b8636e92a5a5a455989bc5bc870
thaynar96 <thaynarbo@gmail.com>
2021-09-03 22:11:02 -0300

    porta do env

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 app.use(express.json());
 
-const Conn = require("./models/conn/conn");
-Conn("localhost", 27017, "");
+const Conn = require('./models/conn/conn');
+Conn('localhost', 27017, '');
 const port = 3000;
 
-const filme = require("./routers/filmes.routes");
-app.use("/filmes", filme);
+const filme = require('./routers/filmes.routes');
+app.use('/filmes', filme);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
 	console.info(`Serviddor rodando na porta ${port}`);
 });
