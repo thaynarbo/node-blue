3f76ff20f58bd4eb2acb6293aa1d7d1f2bbf5193
thaynar96 <thaynarbo@gmail.com>
2021-09-03 22:29:04 -0300

    testando path diferente

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ Conn('localhost', 27017, '');
 const port = 3000;
 
 const filme = require('./routers/filmes.routes');
-app.use('/filmes', filme);
+app.use(`/filmes`, filme);
 
 app.listen(process.env.PORT || port, () => {
 	console.info(`Serviddor rodando na porta ${port}`);
--- routers/filmes.routes.js
+++ routers/filmes.routes.js
@@ -1,72 +1,75 @@
 // buscando o objeto express
-const express = require("express");
+const express = require('express');
 // a constante router está recebendo uma função
 const router = express.Router();
 // importando o modelo criado em filmes.js
-const Filme = require("../models/filmes");
+const Filme = require('../models/filmes');
 
 // criando a rota para adicionar filmes
 // no metodo post o primeiro argumento é o endpoint e o segundo uma função que precisa do req e res como parametro para poder responder o endpoint
-router.post("/add", async (req, res) => {
+router.post('/add', async (req, res) => {
 	const filme = await Filme.create(req.body);
 	filme
 		.save()
 		.then(() => {
-			res.status(200).send("Filme adicionado com sucesso");
+			res.status(200).send('Filme adicionado com sucesso');
 		})
 		.catch((err) => {
-			res.status(400).send("Envio com problemas");
+			res.status(400).send('Envio com problemas');
 		});
 });
 
-router.get("/", async (req, res) => {
-	await Filme.find({})
-		.then((filme) => {
-			res.status(200).send(filme);
-		})
-		.catch((err) => {
-			res.status(400).send("Envio com problemas");
-			console.log(err);
-		});
+router.get('/', async (req, res) => {
+	const pathName = req.url;
+	if (pathName === '/' || pathName === '/overview') {
+		await Filme.find({})
+			.then((filme) => {
+				res.status(200).send(filme);
+			})
+			.catch((err) => {
+				res.status(400).send('Envio com problemas');
+				console.log(err);
+			});
+	}
 });
 
-router.get("/findById/:name", async (req, res) => {
+router.get('/findById/:name', async (req, res) => {
 	await Filme.find({ nome: req.params.name })
 		.then((musica) => {
 			res.status(200).send(filme);
 		})
 		.catch((err) => {
-			res.status(400).send("Envio com problemas");
+			res.status(400).send('Envio com problemas');
 		});
 });
 
-router.get("/findByName/:name", async (req, res) => {
+router.get('/findByName/:name', async (req, res) => {
 	await Filme.find({ nome: req.params.name })
 		.then((filme) => {
 			res.status(200).send(filme);
 		})
 		.catch((err) => {
-			res.status(400).send("Envio com problemas");
+			res.status(400).send('Envio com problemas');
 		});
 });
 
-router.put("/update/:id", async (req, res) => {
+router.put('/update/:id', async (req, res) => {
 	await Filme.updateOne({ _id: req.params.id }, req.body)
 		.then(() => {
-			res.status(200).send("Atualizado com sucesso");
+			res.status(200).send('Atualizado com sucesso');
 		})
 		.catch((err) => {
-			res.status(400).send("Envio com problemas");
+			res.status(400).send('Envio com problemas');
 		});
 });
 
-router.delete("/delete/:id", async (req, res) => {
+router.delete('/delete/:id', async (req, res) => {
 	await Filme.deleteOne({ _id: req.params.id })
 		.then(() => {
-			res.status(200).send("Deletado com sucesso");
+			res.status(200).send('Deletado com sucesso');
 		})
 		.catch((err) => {
-			res.status(400).send("Envio com problemas");
+			res.status(400).send('Envio com problemas');
 		});
 });
 
